174e61c34255a6cd4d7d10213e5f426923fddc66
DESKTOP-6MVAK4S\Admin <ivy-soft@abv.bg>
2017-10-01 17:04:42 +0300

    Added data and models projects

---
+++ Ivysoft.OnlineSystem/Ivysoft.OnlineSystem.Data/OnlineSystemDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Ivysoft.OnlineSystem.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ivysoft.OnlineSystem.Data
+{
+    public class OnlineSystemDbContext : IdentityDbContext<Customer>
+    {
+        public OnlineSystemDbContext()
+            : base("LocalConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static OnlineSystemDbContext Create()
+        {
+            return new OnlineSystemDbContext();
+        }
+    }
+}
---
+++ Ivysoft.OnlineSystem/Ivysoft.OnlineSystem.Models/Contracts/IAuditable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ivysoft.OnlineSystem.Models.Contracts
+{
+    public interface IAuditable
+    {
+    }
+}
