ecf6c6ff52cd9b02948d2a9a4bb09e7cf021a501
DESKTOP-6MVAK4S\Admin <ivy-soft@abv.bg>
2017-10-01 18:10:37 +0300

    Update all packages

--- Ivysoft.OnlineSystem/Ivysoft.OnlineSystem.Web/Controllers/AccountController.cs
+++ Ivysoft.OnlineSystem/Ivysoft.OnlineSystem.Web/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Ivysoft.OnlineSystem.Web.Models;
+using Ivysoft.OnlineSystem.Models;
 
 namespace Ivysoft.OnlineSystem.Web.Controllers
 {
@@ -151,7 +152,7 @@ namespace Ivysoft.OnlineSystem.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new Customer { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
@@ -367,7 +368,7 @@ namespace Ivysoft.OnlineSystem.Web.Controllers
                 {
                     return View("ExternalLoginFailure");
                 }
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new Customer { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
